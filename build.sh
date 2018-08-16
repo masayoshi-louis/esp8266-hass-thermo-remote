def63b602ce1dce2b5a08eab9f2caec7a5a061cd
masayoshi-louis <takamachi660@gmail.com>
2018-08-16 09:09:44 -0700

    improve build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+PROJ_DIR=`pwd`
+MPY_DIR=$PROJ_DIR/../micropython/ports/esp8266
+
+cd $MPY_DIR
+make clean-frozen
+micropython -m upip install -p modules micropython-urequests
+micropython -m upip install -p modules micropython-umqtt.robust
+
+cd $PROJ_DIR
+
+cp -t ../micropython/ports/esp8266/modules \
+  app.py \
+  bme280.py \
+  hass.py \
+  model.py \
+  mqtt.py \
+  ssd1306.py \
+  thingflow.py \
+  wifi.py \
+  writer.py
+
+cp -r font ../micropython/ports/esp8266/modules
+
+cd $MPY_DIR
+make clean
+make
--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,8 @@
+./build.sh
+
 PROJ_DIR=`pwd`
 MPY_DIR=$PROJ_DIR/../micropython/ports/esp8266
-
 cd $MPY_DIR
-make clean-frozen
-micropython -m upip install -p modules micropython-urequests
-micropython -m upip install -p modules micropython-umqtt.robust
-
-cd $PROJ_DIR
-
-cp -t ../micropython/ports/esp8266/modules app.py \
-  bme280.py \
-  hass.py \
-  model.py \
-  mqtt.py \
-  ssd1306.py \
-  thingflow.py \
-  wifi.py \
-  writer.py
-
-cp -r font ../micropython/ports/esp8266/modules
-
-cd $MPY_DIR
-make clean
-make
 
 sudo esptool.py --port /dev/ttyUSB0 erase_flash
 sudo make PORT=/dev/ttyUSB0 deploy
