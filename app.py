28dbb4c9a63484388c310fce58c35fc2fea1edea
luyi <luyi@newtranx.com>
2018-08-16 13:29:46 +0800

    model

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ import model
 import mqtt
 from config import *
 from hass import ThermostatAPI as HassThermostatAPI
-from thingflow import Scheduler
+from thingflow import Scheduler, Output
+
+dht_sensor = None
 
 
 def main():
+    global dht_sensor
     hass_api = hass.API(HASS_BASE_URL, api_password=HASS_PASSWORD)
     hass_thermo = HassThermostatAPI(hass_api, HASS_THERMOSTAT_ID)
     sched = Scheduler()
@@ -21,6 +24,9 @@ def main():
         time.sleep_ms(500)
         cur_state = hass_thermo.get_state()
     print("[HASS] Connected")
+
+    # max_temp = cur_state.attributes.max_temp
+    # min_temp = cur_state.attributes.min_temp
 
     model.init(cur_state)
 
@@ -38,7 +44,7 @@ def main():
 
     dht_sensor = DHTSensor(Pin(PIN_DHT))
 
-    sched.schedule_sensor(dht_sensor, 10, t_sensor_mqtt, h_sensor_mqtt)
+    sched.schedule_sensor(dht_sensor, 10, t_sensor_mqtt, h_sensor_mqtt, DHT2Model())
 
     sched.run_forever()
 
@@ -51,4 +57,11 @@ class DHTSensor:
 
     def sample(self):
         self.s.measure()
-        return [self.s.temperature(), self.s.humidity()]
+        result = [self.s.temperature(), self.s.humidity()]
+        print("[DHT] T = {} {}, H = {} %", result[0], TEMPERATURE_UNIT, result[1])
+        return result
+
+
+class DHT2Model(Output):
+    def on_next(self, x):
+        model.instance.set_current_humidity(x[1])
--- model.py
+++ model.py
@@ -11,20 +11,23 @@ ATTR_SETPOINT = 'temperature'
 ATTR_CURRENT_TEMPERATURE = 'current_temperature'
 ATTR_OP_MODE = 'operation_mode'
 ATTR_STATE = 'state'
-ATTR_CURRENT_HUMIDITY = 'current_humidity'
 
 
 class ThermostatModel:
-    __slots__ = [ATTR_SETPOINT, ATTR_CURRENT_TEMPERATURE, ATTR_OP_MODE, ATTR_STATE, ATTR_CURRENT_HUMIDITY, 'listeners']
+    __slots__ = [ATTR_SETPOINT, ATTR_CURRENT_TEMPERATURE, ATTR_OP_MODE, ATTR_STATE, 'current_humidity', 'listeners']
 
     def __init__(self):
         self.listeners = []
+        self.current_humidity = 0.0
+        self.temperature = 0
+        self.current_temperature = 0
+        self.operation_mode = OP_MODE_OFF
+        self.state = STATE_OFF
 
     def update(self, data: dict):
         for key, value in data.items():
             setattr(self, key, value)
-        for notify in self.listeners:
-            notify()
+        self.notify_listeners()
 
     def update_by_mqtt(self, topic: str, value: str):
         attr = topic[topic.rfind('/') + 1:]
@@ -32,11 +35,18 @@ class ThermostatModel:
             setattr(self, attr, float(value))
         elif attr in {ATTR_OP_MODE, ATTR_STATE}:
             setattr(self, attr, value)
-        for notify in self.listeners:
-            notify()
+        self.notify_listeners()
+
+    def set_current_humidity(self, value: float):
+        self.current_humidity = value
+        self.notify_listeners()
 
     def add_listener(self, l):
         self.listeners.append(l)
+
+    def notify_listeners(self):
+        for notify in self.listeners:
+            notify()
 
 
 def init(init_data: dict):
